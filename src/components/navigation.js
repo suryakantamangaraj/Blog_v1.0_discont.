857cf6bfe7f551f510ea6b5a4291c5733cbc43ec
suryakantamangaraj <surya.socialnetworking@gmail.com>
2020-11-16 19:33:36 +0530

    navigation modified

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,7 +4,6 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
   
